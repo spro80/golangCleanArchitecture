5bfbaf1897a251d1749da3da79023af975506fef
David Diaz <daviddiaz@M-C02FH1TDMD6P.local>
2022-06-08 13:04:45 -0400

    add new variables for config

--- app/shared/config/config.go
+++ app/shared/config/config.go
@@ -3,9 +3,12 @@ package config
 import "os"
 
 type ConfigInterface interface {
-	Handler() (ConfigHandler, error)
+	//Handler() (ConfigHandler, error)
 	GetPort() (string, error)
 	GetUrl() (string, error)
+	GetVersionApp() (string, error)
+	GetEnvironment() (string, error)
+	GetRegion() (string, error)
 }
 
 type ConfigHandler struct {
@@ -17,14 +20,15 @@ func NewConfig() *ConfigHandler {
 	return &ConfigHandler{}
 }
 
+/*
 func (c ConfigHandler) Handler() (ConfigHandler, error) {
 
-	/*
+
 		port := os.Getenv("port")
 		url := os.Getenv("url")
 		fmt.Printf("Port: %s", port)
 		fmt.Printf("Url: %s", url)
-	*/
+
 	port, _ := c.GetPort()
 	url, _ := c.GetUrl()
 
@@ -34,14 +38,29 @@ func (c ConfigHandler) Handler() (ConfigHandler, error) {
 	}
 
 	return config, nil
-}
+}*/
 
 func (c ConfigHandler) GetPort() (string, error) {
-	port := os.Getenv("port") //"9091"
+	port := os.Getenv("PORT")
 	return port, nil
 }
 
 func (c ConfigHandler) GetUrl() (string, error) {
-	url := os.Getenv("port") //"localhost"
+	url := os.Getenv("URL")
 	return url, nil
+}
+
+func (c ConfigHandler) GetVersionApp() (string, error) {
+	versionApp := os.Getenv("VERSION_APP")
+	return versionApp, nil
+}
+
+func (c ConfigHandler) GetEnvironment() (string, error) {
+	environment := os.Getenv("ENVIRONMENT")
+	return environment, nil
+}
+
+func (c ConfigHandler) GetRegion() (string, error) {
+	region := os.Getenv("REGION")
+	return region, nil
 }

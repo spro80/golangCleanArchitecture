c921c92734a5a344986efd8ed3bdf6fab9947812
David Diaz <daviddiaz@M-C02FH1TDMD6P.local>
2022-06-25 20:48:59 -0400

    feature: add logs and add parameters of entity user

--- app/infraestructure/controllers/registerUserController/register_user_controller.go
+++ app/infraestructure/controllers/registerUserController/register_user_controller.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 
 	"github.com/spro80/golangCleanArchitecture/app/application/useCase/registerUserUseCase"
+	"github.com/spro80/golangCleanArchitecture/app/domain/entities"
 )
 
 type RegisterUserControllerInterface interface {
-	HandlerRegisterUserController() error
+	HandlerRegisterUserController(u entities.User) error
 }
 
 type RegisterUserControllerHandler struct {
@@ -18,15 +19,19 @@ func NewRegisterUserController(useCase registerUserUseCase.RegisterUserUseCaseIn
 	return &RegisterUserControllerHandler{useCase: useCase}
 }
 
-func (r *RegisterUserControllerHandler) HandlerRegisterUserController() error {
+func (r *RegisterUserControllerHandler) HandlerRegisterUserController(u entities.User) error {
 	fmt.Println("[register_user_controller] Init in HandlerRegisterUserController")
 	fmt.Println("[register_user_controller] Calling HandlerRegisterUserUseCase")
 
-	err := r.useCase.HandlerRegisterUserUseCase()
+	//fmt.Printf("[register_user_controller] UserName: [%v]", u.Username)
+	//fmt.Printf("[register_user_controller] UserName: [%v]", u.Username)
+
+	userEntity, err := r.useCase.HandlerRegisterUserUseCase(u)
 	if err != nil {
 		fmt.Printf("[register_user_controller] Error: [%s]", err.Error())
 	}
 
+	fmt.Println("[register_user_controller] userEntity: [%v]", userEntity)
 	fmt.Println("[register_user_controller] HandlerRegisterUserUseCase was called successfully")
 	fmt.Println("[register_user_controller] End in HandlerRegisterUserController")
 

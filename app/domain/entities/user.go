26be1aad1e4f0af76c33bad7453efa969df96d7c
David Diaz <daviddiaz@M-C02FH1TDMD6P.local>
2022-06-25 21:05:35 -0400

    feature: add validation for email

--- app/domain/entities/user.go
+++ app/domain/entities/user.go
@@ -5,7 +5,7 @@ type User struct {
 	Rut       string `json:"rut" validate:"required"`
 	FirstName string `json:"firstName" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
-	Email     string `json:"email" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
 	UserName  string `json:"userName" validate:"required"`
 	Password  string `json:"password" validate:"required"`
 }

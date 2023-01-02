7dd8db27f418ad9d627762e7c3d0c5c6783e5c51
David Diaz <david.diaz2@walmart.com>
2023-01-01 22:36:27 -0300

    handle response

--- app/shared/utils/response/response.go
+++ app/shared/utils/response/response.go
@@ -1,1 +1,26 @@
 package response
+
+type ResponseInterface interface {
+	HandlerCreateResponseSuccess(status int, statusDes string, data string, errorDes string) ResponseStruct
+}
+
+type ResponseStruct struct {
+	Status            int    `json:"status"`
+	StatusDescription string `json:"statusDescription"`
+	Data              string `json:"data"`
+	Error             string `json:"error"`
+}
+
+func NewResponse() *ResponseStruct {
+	return &ResponseStruct{}
+}
+
+func (r *ResponseStruct) HandlerCreateResponseSuccess(status int, statusDes string, data string, errorDes string) ResponseStruct {
+	response := ResponseStruct{
+		Status:            status,
+		StatusDescription: statusDes,
+		Data:              data,
+		Error:             errorDes,
+	}
+	return response
+}

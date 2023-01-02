8eb988007db1bd575a37ead331ae82e8294348da
David Diaz <david.diaz2@walmart.com>
2023-01-01 22:43:33 -0300

    add interfaces for gateway

--- app/domain/interfaces_gateway/gateways_user.go
+++ app/domain/interfaces_gateway/gateways_user.go
@@ -1,9 +1,12 @@
-package interfaces
+package interfaces_gateway
 
 import (
-	user_entities_interface "github.com/spro80/golangCleanArchitecture/app/domain/entity/user/interfaces"
+	"context"
+	user_entities_interface "github.com/spro80/golangCleanArchitecture/app/domain/entity/user_entity/interfaces"
 )
 
 type RepositoryGatewayInterface interface {
+	FindAllUsers(ctx context.Context) ([]user_entities_interface.UserEntityInterface, error)
 	FindUserByRut(rut string) (user_entities_interface.UserEntityInterface, error)
+	SaveUser(ctx context.Context, user user_entities_interface.UserEntityInterface) (user_entities_interface.UserEntityInterface, error)
 }
